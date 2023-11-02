ffa078c79145d6d809a7b028eba2b698af7577e3
chandan0134 <chandanma39@gmail.com>
2023-11-03 01:59:27 +0530

    commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,19 +4,19 @@ import React from 'react'
 function NavBar() {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg bg-danger">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">NewsDonkey</a>
+    <a className="navbar-brand text-light" href="/">NewsDonkey</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
+          <a className="nav-link active text-light" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">Link</a>
+          <a className="nav-link text-light" href="/">Link</a>
         </li>
        
       </ul>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,13 +4,13 @@ import News from  "./News";
 function NewsItem(props) {
   return (
     <div>
-      <div className="card" style={{ width: "18rem" }}>
-        <img src={props.imageURL} className="card-image-top" alt="..." />
+      <div className="card" style={{ width: "18rem" ,margin: " 2rem",height : "400px"}}>
+        <img src={props.imageURL} className="card-image-top h-30" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{props.title}...</h5>
           <p className="card-text">{props.description}...</p>
           <a href={props.newsURL} target="blank" className="btn btn-sn btn-primary">
-            Go somewhere
+            Read News
           </a>
         </div>
       </div>
